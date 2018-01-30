a51d80aeb4fb4127e2741bb29ca443ebfa0eea06
Ryan Hristovski <rh12wb@brocku.ca>
2018-01-30 14:05:19 -0500

    Initial commit

---
+++ src/main/java/hello/SpringBootWebApplication.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+
+}

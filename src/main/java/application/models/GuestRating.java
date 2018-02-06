060f5d6e54c584cef4742573a14be17905b18cbc
nikolafilipovic <nf11dq@brocku.ca>
2018-02-06 17:13:27 -0500

    I have added the methods and variables as described in the object classes file in our shared drive

--- src/main/java/application/models/GuestRating.java
+++ src/main/java/application/models/GuestRating.java
@@ -1,4 +1,24 @@
 package application.models;
 
 public class GuestRating {
+
+    private int totalRating;
+    private String comments;
+
+    public int getTotalRating() {
+        return totalRating;
+    }
+
+    public void setTotalRating(int totalRating) {
+        this.totalRating = totalRating;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
 }

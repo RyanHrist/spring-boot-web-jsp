f2f8773de783672ef6f19fd452ab7817a2ce7cba
nikolafilipovic <nf11dq@brocku.ca>
2018-02-06 17:09:14 -0500

    I have added the methods and variables for host and guest ratings as shown on the object classes file we have in the shared drive

--- src/main/java/application/models/HostRating.java
+++ src/main/java/application/models/HostRating.java
@@ -1,4 +1,38 @@
 package application.models;
 
 public class HostRating {
+    private int totalRating;
+    private int mealRating;
+    private int cleanlinessRating;
+    private int hospitalityRating;
+    private String comments;
+
+    public int getTotalRating() {
+        return totalRating;
+    }
+
+    public int getMealRating() {
+        return mealRating;
+    }
+
+    public int getCleanlinessRating() {
+        return cleanlinessRating;
+    }
+
+    public int getHospitalityRating() {
+        return hospitalityRating;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setTotalRating(int totalRating) {
+        this.totalRating = totalRating;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
 }

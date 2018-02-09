35f3032db1b7de2125d160012a7d42c4c73bd1ee
Jordan <jm14uw@brocku.ca>
2018-02-09 15:09:58 -0500

    Created Host variables and methods

--- src/main/java/application/models/Host.java
+++ src/main/java/application/models/Host.java
@@ -1,4 +1,46 @@
 package application.models;
 
+import application.models.enumerations.Country;
+import application.models.enumerations.Province;
+
 public class Host {
+    private HostRating hostRating;
+    private Meals meal;
+    private  Location location;
+    private User previousGuests;
+    private double duration;
+    private Date cancellation;
+
+    public HostRating getHostRating() {
+        return hostRating;
+    }
+
+    public Meals getMeals() {
+        return meals;
+    }
+
+public Location getLocation(){
+    return location;
+}
+
+public double getDuration() {
+    return duration;
+    }
+    public User getPreviousGuests(){
+        return previousGuests;
+    }
+    public void setMeals (Meals meal){
+        this.meal = meal;
+    }
+    public void setLocation (Location location){
+        this.location = location;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+    public void setCancellation(Date cancellation){
+        this.cancellation = cancellation;
+    }
+    //public void calculateRating(){}
 }

745c9be06ea066c2201f07377c287ececd99bbe0
nikolafilipovic <nf11dq@brocku.ca>
2018-02-28 13:43:24 -0500

    Added ProfileView function

---
+++ src/main/java/application/models/ProfileView.java
@@ -0,0 +1,16 @@
+package application.models;
+public class ProfileView {
+
+    private User user;
+
+    private void editAccount(){}
+
+    private void showRating(){}
+
+    private void showHostRating(){}
+
+    private void showFutureHostMeals(){}
+
+
+
+}
